8c92c182f35615f4b8b84fdd99de57d0a763ab33
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-04-05 19:58:47 -0700

    Drills 06

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,39 @@
 
 
-function removeClassFromElement(){
-	
+function removeClassFromElement(element, className){
+  $(element).removeClass(className);
 }
 
-function toggleClassOnElement(){
-	
+function toggleClassOnElement(element, className){
+  $(element).toggleClass(className);
 }
 
-function hideElements(  ){
-	
+function hideElements(element, type){
+  switch(type){
+    case 'hide':
+      $(element).hide();
+      break;
+    case 'removeChildren':
+      $(element).empty();
+      break;
+    case 'removeSelf':
+      $(element).remove();
+      break;
+    default:
+      break;
+    }
 }
 
-function addAttributeToElement( ){
-	
+function addAttributeToElement(element, attribute, value){
+  $(element).attr(attribute, value);
 }
 
-function putPosInElement(  ){
-	
+function putPosInElement(element){
+ let position = $(element).position();
+ let result = {
+   "x": position.left,
+   "y": position.top
+ }
+ $(element).html(`x: ${result.x}px<br>y: ${result.y}px`)
+ return result;
 }
-
-
