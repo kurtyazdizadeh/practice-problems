a71c09aad1bf89dcf18a99d804ba1635c1881651
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-06-19 16:32:02 -0700

    updated comments for solution

--- code-signal/first_recurring_character.js
+++ code-signal/first_recurring_character.js
@@ -8,6 +8,9 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
 
 function firstRecurringCharacter(array) {
   const hash = {};
@@ -21,10 +24,7 @@ function firstRecurringCharacter(array) {
   }
 }
 
-console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
-console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]));
-console.log(firstRecurringCharacter([2, 3, 4, 5]));
-console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
-//Bonus... What if we had this:
-// [2,5,5,2,3,5,1,2,4]
-// return 5 because the pairs are before 2,2
+console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4])); //2
+console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4])); //1
+console.log(firstRecurringCharacter([2, 3, 4, 5])); //undefined
+console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4])); //5
